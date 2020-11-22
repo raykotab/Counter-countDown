e76d7ec3aca65640a513fd653b9449a526b73822
Alvaro <astaboada7@gmail.com>
2020-11-23 00:13:07 +0100

    blockiert

--- src/CountDownFunction.js
+++ src/CountDownFunction.js
@@ -1,19 +1,31 @@
+'use strict';
 
+//let countDownDate = document.getElementById("apocalypseDate").value;
 
+window.addEventListener("DOMContentLoaded", function(e) {
+    console.log("page loaded");
+    const userBetDate = locaStorage.getItem('date');
+    userBetDate.seAttribute("value", )
+});
 
-  var countDownDate = new Date("Nov 17, 2022 14:36:59").getTime();
-  var x = setInterval(function() {
-      var now = new Date().getTime();
-      var distance = countDownDate - now;
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+let endDate = document.getElementById("date").value;
+endDate.addEventListeneer
+console.log(typeof endDate);
+
+let countDownDate = new Date(endDate).getTime();
+
+  let x = setInterval(function() {
+      let now = new Date().getTime();
+      let distance = countDownDate - now;
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       document.getElementById("demo").innerHTML = days + "d" + " " + hours + "h" + " " + minutes + "m" + " " + seconds + "s";
 
       if (distance < 0) {
           clearInterval(x);
-          document.getElementById("demo").innerHTML = "EXTINGISHED";
+          document.getElementById("demo").innerHTML = "EXTINGUISHED";
       }
   },1000);
