a30043b7b9f5a3a482dc9d672c59cc154199d57e
Alvaro <astaboada7@gmail.com>
2020-11-24 20:10:06 +0100

    works, reset feature to do

--- src/CountDownFunction.js
+++ src/CountDownFunction.js
@@ -1,31 +1,44 @@
-'use strict';
+'use strict'
 
-//let countDownDate = document.getElementById("apocalypseDate").value;
+    function resetDemo() {
+    
+        let timeNow = new Date().getTime();
+        console.log(resetedDate);
+        document.getElementById("demo").innerHTML = "timeNow";
+        
+    } 
+    resetDemo();
 
-window.addEventListener("DOMContentLoaded", function(e) {
-    console.log("page loaded");
-    const userBetDate = locaStorage.getItem('date');
-    userBetDate.seAttribute("value", )
+    let button = document.getElementById("setEndDate")
+
+    button.addEventListener("click", function(e) {
+
+    console.log("button clicked");
+
+    let endDate = document.getElementById("date").value;
+    
+    let countDownDate = new Date(endDate).getTime();
+    
+    let x = setInterval(function() {
+
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+        document.getElementById("demo").innerHTML = days + "d" + " " + hours + "h" + " " + minutes + "m" + " " + seconds + "s";
+    
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXTINGUISHED";
+        }
+
+    },1000);
+
 });
 
-let endDate = document.getElementById("date").value;
-endDate.addEventListeneer
-console.log(typeof endDate);
 
-let countDownDate = new Date(endDate).getTime();
 
-  let x = setInterval(function() {
-      let now = new Date().getTime();
-      let distance = countDownDate - now;
-      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      document.getElementById("demo").innerHTML = days + "d" + " " + hours + "h" + " " + minutes + "m" + " " + seconds + "s";
-
-      if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("demo").innerHTML = "EXTINGUISHED";
-      }
-  },1000);
